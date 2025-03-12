ff6953a8d2e893b6eccd02d664693c8c74646537
Amir Hossein <140048011+Amir012012@users.noreply.github.com>
2025-03-12 18:25:02 +0330

    Update app.js

--- app.js
+++ app.js
@@ -2,39 +2,41 @@ let hr = document.getElementById("hr");
 let mn = document.getElementById("mn");
 let sc = document.getElementById("sc");
 
-setInterval(() => {
-  let Day = new Date();
-  let hh = Day.getHours() * 30;
-  let mm = Day.getMinutes() * 6;
-  let ss = Day.getSeconds() * 6;
+let hours = document.getElementById("hour");
+let minute = document.getElementById("minutes");
+let second = document.getElementById("seconds");
+let pmam = document.getElementById("pmam");
 
-  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
-  mn.style.transform = `rotateZ(${mm}deg)`;
-  sc.style.transform = `rotateZ(${ss}deg)`;
+function updateClock() {
+  let now = new Date();
+  let milliseconds = now.getMilliseconds();
+  let seconds = now.getSeconds() + milliseconds / 1000;
+  let minutes = now.getMinutes() + seconds / 60;
+  let hoursValue = (now.getHours() % 12) + minutes / 60;
 
-  let hours = document.getElementById("hour");
-  let minute = document.getElementById("minutes");
-  let second = document.getElementById("seconds");
-  let pmam = document.getElementById("pmam");
+  let ssDeg = seconds * 6;
+  let mmDeg = minutes * 6;
+  let hhDeg = hoursValue * 30;
 
-  let h = new Date().getHours();
-  let m = new Date().getMinutes();
-  let s = new Date().getSeconds();
+  sc.style.transform = `rotateZ(${ssDeg}deg)`;
+  mn.style.transform = `rotateZ(${mmDeg}deg)`;
+  hr.style.transform = `rotateZ(${hhDeg}deg)`;
 
-  let am = h >= 12 ? 'PM' : 'AM'
+  let h = now.getHours();
+  let m = now.getMinutes();
+  let s = now.getSeconds();
+  let amPm = h >= 12 ? "PM" : "AM";
 
-  if(h > 12){
-    h = h - 12
+  if (h > 12) {
+    h -= 12;
   }
 
-  h = h < 10 ? "0" + h : h;
-  m = m < 10 ? "0" + m : m;
-  s = s < 10 ? "0" + s : s;
+  hours.innerHTML = h < 10 ? "0" + h : h;
+  minute.innerHTML = m < 10 ? "0" + m : m;
+  second.innerHTML = s < 10 ? "0" + s : s;
+  pmam.innerHTML = amPm;
 
-  hours.innerHTML = h;
-  minute.innerHTML = m;
-  second.innerHTML = s;
-  pmam.innerHTML = am;
-});
+  requestAnimationFrame(updateClock);
+}
 
-// digital clock
+updateClock();
